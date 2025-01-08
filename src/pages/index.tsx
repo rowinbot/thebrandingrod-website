da9ff70ed4f66862962ba63cdfbeb83a3be03fff
Rowin Hernandez <rowinkaleb@gmail.com>
2025-01-08 14:50:24 +0100

    feat: responsive home page + header and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { LogoAsset } from "@/assets/logo";
+import { Icon } from "@iconify-icon/react";
+import { Poppins, Geist_Mono } from "next/font/google";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+import img from "../../public/img/header-me.jpeg";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const sans = Poppins({
+  variable: "--font-poppins-sans",
+  weight: ["400", "500", "700"],
 });
 
 const geistMono = Geist_Mono({
@@ -11,103 +15,97 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const topBarNavButtons = [
+  { label: "Home", link: "/" },
+  { label: "Book me", link: "https://calendly.com/thebrandingrod/30min" },
+  { label: "Email", link: "email:me@thebrandingrod.com" },
+];
+
+const socialButtons = [
+  { icon: "hugeicons:dribbble", link: "https://dribbble.com/brandingrod" },
+  { icon: "uil:behance", link: "email:me@thebrandingrod.com" },
+  {
+    icon: "hugeicons:instagram",
+    link: "https://www.instagram.com/branding.rod",
+  },
+];
+
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${sans.variable} ${geistMono.variable} grid grid-rows-[auto_1fr_auto] min-h-screen gap-16 font-sans`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <nav className="grid grid-cols-1 max-sm:place-items-center lg:grid-cols-[1fr_auto_1fr] max-md:sm:grid-cols-2 md:grid-cols-[180px_1fr_180px] items-center py-8 px-16 sm:py-16 max-sm:gap-6">
+        <Link href="/" className="lg:col-[2] lg:row-[1] max-md:sm:row-span-2">
+          <LogoAsset className="size-28 lg:size-36 transition-all duration-75" />
+        </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex flex-row justify-between lg:gap-10 lg:justify-start lg:col-[1] lg:row-[1] whitespace-nowrap">
+          {topBarNavButtons.map((button) => (
+            <Link
+              key={button.link}
+              href={button.link}
+              target={button.link.startsWith("/") ? undefined : "_blank"}
+              rel="noopener noreferrer"
+              className="text-base font-bold px-2 uppercase"
+            >
+              {button.label}
+            </Link>
+          ))}
         </div>
+        <div className="justify-center sm:justify-end items-center flex flex-row gap-4 lg:col-[3] lg:row-[1] max-md:sm:col-[2]">
+          {socialButtons.map((button) => (
+            <Link
+              key={button.link}
+              href={button.link}
+              target={button.link.startsWith("/") ? undefined : "_blank"}
+              rel="noopener noreferrer"
+              className="p-2 bg-white rounded-full items-center justify-center transition-all duration-300 hover:scale-125"
+            >
+              <Icon icon={button.icon} width={24} />
+            </Link>
+          ))}
+        </div>
+      </nav>
+
+      <main className="flex flex-col gap-8 row-start-2 items-center px-8">
+        <header className="flex flex-col items-center gap-20 text-center">
+          <div className="space-y-4">
+            <h1 className="text-6xl md:text-7xl font-medium">Rodrigo Flores</h1>
+            <p className="whitespace-pre-line font-serif text-4xl md:text-5xl leading-snug">
+              {"Graphic Designer\nBased in Spain"}
+            </p>
+          </div>
+
+          <Image
+            src={img}
+            placeholder="blur"
+            alt="Me"
+            width={485}
+            height={600}
+            className="rounded-full object-cover aspect-[485/600] transition-all duration-75 max-w-full sm:max-md:max-w-[400px]"
+          />
+        </header>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-between px-16 py-8">
+        <p className="text-center">
+          {"© 2024 Rodrigo Flores. All rights reserved."}
+        </p>
+
+        <div className="flex flex-row gap-4">
+          {socialButtons.map((button) => (
+            <Link
+              key={button.link}
+              href={button.link}
+              target={button.link.startsWith("/") ? undefined : "_blank"}
+              rel="noopener noreferrer"
+              className="p-2 bg-white rounded-full items-center justify-center transition-all duration-300 hover:scale-125"
+            >
+              <Icon icon={button.icon} width={24} />
+            </Link>
+          ))}
+        </div>
       </footer>
     </div>
   );
