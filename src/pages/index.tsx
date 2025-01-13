4d2bd3cc112f8ec6ddde76a0632531be86d336e3
Rowin Hernandez <rowinkaleb@gmail.com>
2025-01-13 10:37:10 +0100

    feat: center ig posts on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -145,7 +145,7 @@ export default function Home() {
           </a>
         </h2>
 
-        <ul className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-20">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-20 place-items-center">
           {instagramPosts.map((post) => (
             <InstagramPostButton key={post.label} post={post} />
           ))}
